12ea5e65b7a80372cb7aa2df51ede3764e7b5a3e
Ogi-Git <ognjen25aleksic@gmail.com>
2025-07-07 23:25:42 +0200

    Set main router

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+use axum::Router;
+use axum::routing::get;
+use handlers::fetch_handler::fetch_pools;
+
+mod handlers;
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new().route(
+        "/api/pool-data/{token_mint_a}/{token_mint_b}",
+        get(fetch_pools),
+    );
+
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+
+    axum::serve(listener, app).await.unwrap();
 }
